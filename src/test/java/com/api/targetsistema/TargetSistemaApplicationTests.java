c04582c772299c421c8f7fdc3ce8e7ddb4f4bbc3
PH <dpseutroco@gmail.com>
2024-04-03 16:55:52 -0300

    Desafio TargetSistema

---
+++ src/test/java/com/api/targetsistema/TargetSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.targetsistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
